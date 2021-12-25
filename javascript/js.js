a049ad7cc4884b4f673c95259e659d925c30b70a
Matheus-Venancio <matheusvecordeiro@gmail.com>
2021-12-25 16:28:23 -0300

    Verify input complete

--- javascript/js.js
+++ javascript/js.js
@@ -1,44 +1,67 @@
 
-const btn2 = document.querySelector(".buttoContact")
+/*const btn2 = document.getElementById("btnContact")*/
+const nome = document.getElementById('name')
+const email = document.getElementById('email')
+const phone = document.getElementById('phone')
+const txtarea = document.getElementById('mensagem')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
 const ligthbox = document.createElement('imgHome')
 const images = document.querySelectorAll('img');
 
-/**Envia mensagem pelo email */
-btn2.onclick = function() {
-var Name = document.getElementById("nome");
-var Email = document.getElementById("email");
-var Phone = document.getElementById("phone")
-var Txtarea = document.getElementById("mensagem");
-
-  if(Name.value.length<7){
-    Name.style.backgroundColor="#ff6459"
-    alert("Nome devera ter pelo menos 7 letras")
-    Name.focus();
-    return false;
-  }else{
-    if(Email.value.length<8){
-      Email.style.backgroundColor="#ff6459"
-      alert("Email devera ter pelo menos 8 letras")
-      Email.focus();
-      return false;
-    }else{
-      if(Phone.value.length<13){
-        Phone.style.backgroundColor="#ff6459"
-        alert("Phone devera ter pelo menos 11 numeros")
-        Phone.focus();
-        return false
-      }else{
-      if(Txtarea.value.length<8){
-        Txtarea.style.backgroundColor="#ff6459"
-        alert("Mensagem devera ter pelo menos 8 letras")
-        Txtarea.focus();
-        return false;
-      }
-    }
-   }
-   alert("Mensagem enviada com sucesso")
+/**Verify inputs */
+form.addEventListener('submit', (e) =>{
+  let messages = []
+  if(nome.value === '' ||nome.value == null){
+    messages.push('Preencha o nome')
   }
-};
+
+  if(nome.value.length<=10){
+    messages.push('O nome deve conter pelo menos 10 caracteres')
+  }
+
+  if(email.value.length<=10){
+    messages.push('O emal deve conter pelo menos 10 caracteres')
+  }
+
+  if(phone.value.length<11){
+    messages.push('o telefone tem que ter 11 caracteres')
+  }
+
+  if(messages.length > 0){
+    e.preventDefault()
+    errorElement.innerText= messages.join(', ')
+  }
+})
+
+//Number inputs 
+function numberInput(evt){
+  var theEvent =evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+
+  key = String.fromCharCode(key);
+
+  var regex = /^[0-9]+$/;
+
+  if(!regex.test(key)){
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
+
+function textInput(evt){
+  var theEvent =evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+
+  key = String.fromCharCode(key);
+
+  var regex = /^[a-z]/;
+
+  if(!regex.test(key)){
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
 
 /**Abrir o menu */
 function abrirMenu(){
@@ -95,4 +118,3 @@ function reveal(){
    }
  }
 }
-
